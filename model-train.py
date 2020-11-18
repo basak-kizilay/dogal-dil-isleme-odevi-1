a9917ee7f321ff0e4bd8d9777dd901b190123af7
basak-kizilay <74666977+basak-kizilay@users.noreply.github.com>
2020-11-18 18:20:30 +0300

    Update model-train.py

--- model-train.py
+++ model-train.py
@@ -2,4 +2,4 @@ from imageai.Prediction.Custom import ModelTraining
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
 model_trainer.setDataDirectory("dataset")
-model_trainer.trainModel(num_objects=2, num_experiments=100, enhance_data=True, batch_size=5, show_network_summary=True, save_full_model=True)
+model_trainer.trainModel(num_objects=2, num_experiments=100, enhance_data=True, batch_size=5, show_network_summary=True, save_full_model=False)
